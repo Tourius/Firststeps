e07fe77bc15c1dc2db2b0976e2b730820be80386
Tourius <joshua_wheeler@hotmail.co.uk>
2017-08-10 00:59:20 +0100

    shit

--- Joshsgame/Joshsgame/Program.cs
+++ Joshsgame/Joshsgame/Program.cs
@@ -15,60 +15,28 @@ namespace Joshgame
             while (replay == true)
             {
                 Console.Clear();
-                Console.WriteLine("Welcome to Boop Woop, the reaction game! \nThe rules are simple:\nWhen you see the word Boop press the left key.\nWhen you see the word Woop press the right key.\nIf any other word appears press the down key.\nYou'll have 3 lives, see if you can reach the 100th word! \nWhen you are ready push the Enter key.");
-                do { }
-                while (Console.ReadKey(true).Key != ConsoleKey.Enter);
-                { }
-                Console.WriteLine("Here we go!");
-                Console.Clear();
-                Console.WriteLine("3...");
-                Thread.Sleep(500);
-                Console.WriteLine("2...");
-                Thread.Sleep(500);
-                Console.WriteLine("1...");
-                Thread.Sleep(500);
-                Console.WriteLine("GO!");
-                Thread.Sleep(500);
-                Console.Clear();
+                Intro();
                 int lives = 3;
-                int n = 0;
+                int score = 0;
                 while (lives > 0)
                 {
-                    if (n == 25 || n == 50 || n == 75)
+                    if (score == 25 || score == 50 || score == 75)
                     {
-                        Console.WriteLine("{0} so far. Good job, but watch out those words will disappear faster now!", n);
+                        Console.WriteLine($"{score} so far. Good job, but watch out those words will disappear faster now!");
                         Thread.Sleep(3000);
                     }
                     Console.Clear();
                     Random rng = new Random();
                     int rand = rng.Next(0, 26);
-                    char let = (Char)('a' + rand);
-                    string Ulet = let.ToString().ToUpper();
-                    Console.WriteLine("{0}oop", Ulet);
-                    if (n <= 25)
-                    {
-                        Thread.Sleep(800);
-                        Console.Clear();
-                    }
-                    else if (n > 25 && n <= 50)
-                    {
-                        Thread.Sleep(600);
-                        Console.Clear();
-                    }
-                    else if (n > 50 && n <= 75)
-                    {
-                        Thread.Sleep(400);
-                        Console.Clear();
-                    }
-                    else if (n > 75)
-                    {
-                        Thread.Sleep(200);
-                        Console.Clear();
-                    }
-                    if (Ulet == "B")
+                    char letter1 = (Char)('A' + rand);
+                    Console.WriteLine("{0}oop", letter1);
+                    Speed(score);
+                    if (letter1 == 'B')
                     {
                         if (Console.ReadKey(true).Key == ConsoleKey.LeftArrow)
-                        { }
+                        {
+                            score = (score + 1);
+                        }
                         else
                         {
                             lives = (lives - 1);
@@ -85,10 +53,12 @@ namespace Joshgame
                             }
                         }
                     }
-                    else if (Ulet == "W")
+                    else if (letter1 == 'W')
                     {
                         if (Console.ReadKey(true).Key == ConsoleKey.RightArrow)
-                        { }
+                        {
+                            score = (score + 1);
+                        }
                         else
                         {
                             lives = (lives - 1);
@@ -104,9 +74,11 @@ namespace Joshgame
                             }
                         }
                     }
-                    else if (Ulet != "B" && Ulet != "W")
+                    else if (letter1 != 'B' && letter1 != 'W')
                         if (Console.ReadKey(true).Key == ConsoleKey.DownArrow)
-                        { }
+                        {
+                            score = (score + 1);
+                        }
                         else
                         {
                             lives = (lives - 1);
@@ -125,38 +97,80 @@ namespace Joshgame
                     {
                         break;
                     }
-                    n = (n + 1);
-                    if (n == 100)
+                    if (score == 100)
                     {
                         break;
                     }
 
                 }
-                if (n <= 25)
+                if (score <= 25)
                 {
-                    Console.WriteLine("Aww too bad you only scored {0}, better luck next time. \nIf you'd like to play again push Enter. \nIf you'd like to close the game push any other key.", n);
+                    Console.WriteLine("Aww too bad you scored {0}, better luck next time. \nIf you'd like to play again push Enter. \nIf you'd like to close the game push any other key.", score);
                 }
-                else if (n > 25 && n <= 50)
+                else if (score > 25 && score <= 50)
                 {
-                    Console.WriteLine("Not bad you scored {0}, but you could do better. \nIf you'd like to play again push Enter. \nIf you'd like to close the game push any other key.", n);
+                    Console.WriteLine("Not bad you scored {0}, but you could do better. \nIf you'd like to play again push Enter. \nIf you'd like to close the game push any other key.", score);
                 }
-                else if (n > 50 && n <= 75)
+                else if (score > 50 && score <= 75)
                 {
-                    Console.WriteLine("Way to go {0}, Thats over half way to the top, why not try again? \nIf you'd like to play again push Enter. \nIf you'd like to close the game push any other key.", n);
+                    Console.WriteLine("Way to go {0}, Thats over half way to the top, why not try again? \nIf you'd like to play again push Enter. \nIf you'd like to close the game push any other key.", score);
                 }
-                else if (n > 75 && n < 100)
+                else if (score > 75 && score < 100)
                 {
-                    Console.WriteLine("Wow {0}, almost the top score. One more go to get to the top? \nIf you'd like to play again push Enter. \nIf you'd like to close the game push any other key.", n);
+                    Console.WriteLine("Wow {0}, almost the top score. One more go to get to the top? \nIf you'd like to play again push Enter. \nIf you'd like to close the game push any other key.", score);
                 }
-                else if (n == 100)
+                else if (score == 100)
                 {
-                    Console.WriteLine("{0}!!! CONGRATULATIONS! You've beat the game, but could you do it again? \nIf you'd like to play again push Enter. \nIf you'd like to close the game push any other key.", n);
+                    Console.WriteLine("{0}!!! CONGRATULATIONS! You've beat the game, but could you do it again? \nIf you'd like to play again push Enter. \nIf you'd like to close the game push any other key.", score);
                 }
                 if (Console.ReadKey(true).Key != ConsoleKey.Enter)
                 {
                     replay = false;
                 }
             }
+        }
+
+        private static void Speed(int n)
+        {
+            if (n <= 25)
+            {
+                Thread.Sleep(800);
+                Console.Clear();
+            }
+            else if (n > 25 && n <= 50)
+            {
+                Thread.Sleep(600);
+                Console.Clear();
+            }
+            else if (n > 50 && n <= 75)
+            {
+                Thread.Sleep(400);
+                Console.Clear();
+            }
+            else if (n > 75)
+            {
+                Thread.Sleep(200);
+                Console.Clear();
+            }
+        }
+
+        private static void Intro()
+        {
+            Console.WriteLine("Welcome to Boop Woop, the reaction game! \nThe rules are simple:\nWhen you see the word Boop press the left key.\nWhen you see the word Woop press the right key.\nIf any other word appears press the down key.\nYou'll have 3 lives, see if you can reach the 100th word! \nWhen you are ready push the Enter key.");
+            do { }
+            while (Console.ReadKey(true).Key != ConsoleKey.Enter);
+            { }
+            Console.WriteLine("Here we go!");
+            Console.Clear();
+            Console.WriteLine("3...");
+            Thread.Sleep(500);
+            Console.WriteLine("2...");
+            Thread.Sleep(500);
+            Console.WriteLine("1...");
+            Thread.Sleep(500);
+            Console.WriteLine("GO!");
+            Thread.Sleep(500);
+            Console.Clear();
         }
     }
 }
